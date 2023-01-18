db019ed02e1b014db1fdee2760217eaf275b0569
ansel <79257300125@ya.ru>
2023-01-18 04:49:02 +0300

    Resort items on event from collection

--- osu.Framework/Graphics/Containers/RearrangeableListContainer.cs
+++ osu.Framework/Graphics/Containers/RearrangeableListContainer.cs
@@ -110,6 +110,10 @@ namespace osu.Framework.Graphics.Containers
                     removeItems(e.OldItems);
                     addItems(e.NewItems);
                     break;
+
+                case NotifyCollectionChangedAction.Move:
+                    sortItems();
+                    break;
             }
         }
 
